a34604452e59d441b9527c9c7c7519dacdc57961
Alexander Böhm <alexander.boehm@malbolge.net>
2024-03-14 17:09:47 +0100

    1st working version of echo char device

--- rust/kernel/miscdev.rs
+++ rust/kernel/miscdev.rs
@@ -2,12 +2,21 @@
 
 //! Miscellaneous devices.
 //!
-use core::{ffi::c_void, marker::PhantomData, pin::Pin};
+use core::{
+    ffi::c_void,
+    marker::{PhantomData, PhantomPinned},
+    mem::MaybeUninit,
+    ops::Deref,
+    ops::DerefMut,
+    pin::Pin,
+};
 
 use crate::{c_str, pr_info};
 use alloc::{boxed::Box, vec::Vec};
-use kernel::bindings::{
-    file, inode, misc_deregister, misc_register, miscdevice, MISC_DYNAMIC_MINOR,
+use kernel::{
+    bindings::{file, inode, misc_deregister, misc_register, miscdevice, MISC_DYNAMIC_MINOR},
+    prelude::*,
+    types::ForeignOwnable,
 };
 
 /// Registration for miscellaneous device
@@ -32,8 +41,10 @@ pub struct Registration<T: MiscDev> {
     registered: bool,
     /// Holds device information
     pub miscdev: miscdevice,
-    /// Holds the miscellaneous device callback implementation
-    marker: PhantomData<T>,
+    /// Hold device state
+    open_data: MaybeUninit<T::OpenData>,
+    /// Unpin isn't allowed for `Registration`
+    _pin: PhantomPinned,
 }
 
 impl<T: MiscDev> Default for Registration<T> {
@@ -41,18 +52,23 @@ impl<T: MiscDev> Default for Registration<T> {
         Registration {
             registered: false,
             miscdev: miscdevice::default(),
-            marker: PhantomData,
+            open_data: MaybeUninit::uninit(),
+            _pin: PhantomPinned,
         }
     }
 }
 
-impl<T: MiscDev> Registration<T> {
+impl<T> Registration<T>
+where
+    T: MiscDev,
+    T::Data: 'static,
+{
     #[allow(dead_code)]
     const VTABLE: kernel::bindings::file_operations = kernel::bindings::file_operations {
         open: Some(Self::open_callback),
-        release: None,
+        release: Some(Self::release_callback),
         read: Some(Self::read_callback),
-        write: None,
+        write: Some(Self::write_callback),
         llseek: Some(kernel::bindings::noop_llseek),
         check_flags: None,
         compat_ioctl: None,
@@ -84,38 +100,61 @@ impl<T: MiscDev> Registration<T> {
         write_iter: None,
     };
 
-    #[allow(dead_code)]
-    /// Register a miscellaneous device implementation
-    pub fn new_pinned() -> kernel::error::Result<Pin<Box<Registration<T>>>> {
-        pr_info!("box = {:p}\n", Box::try_new(1)?);
-        Ok(Pin::from(Box::try_new(Registration::default())?))
+    pub fn new_pinned(data: T::OpenData) -> Result<Pin<Box<Self>>> {
+        let registration = Registration::default();
+        let registration = Box::try_new(registration)?;
+        pr_info!("Registration place at {:p}", &registration);
+        let mut registration = Pin::from(registration);
+        Self::register(registration.as_mut(), data)?;
+        Ok(registration)
     }
 
-    #[allow(dead_code)]
-    /// Register a miscellaneous device implementation
-    pub fn register(self: Pin<&mut Self>) -> kernel::error::Result<()> {
-        let this = unsafe { self.get_unchecked_mut() };
-        if this.registered {
+    /// Register the device
+    fn register(self: Pin<&mut Self>, data: T::OpenData) -> Result<()> {
+        let registration = unsafe { self.get_unchecked_mut() };
+        if registration.registered {
             // Already registered.
             return Err(kernel::prelude::EINVAL);
         }
+        registration.registered = true;
 
-        this.miscdev.minor = MISC_DYNAMIC_MINOR as i32;
-        this.miscdev.name = c_str!("rchrdev").as_char_ptr();
-        this.miscdev.fops = &Self::VTABLE;
-        let res = unsafe { misc_register(&mut this.miscdev) };
+        registration.miscdev.minor = MISC_DYNAMIC_MINOR as i32;
+        registration.miscdev.name = c_str!("rchrdev").as_char_ptr();
+        registration.miscdev.fops = &Self::VTABLE;
+        let res = unsafe { misc_register(&mut registration.miscdev) };
         kernel::error::to_result(res)?;
-        this.registered = true;
+        registration.open_data.write(data);
+        pr_info!(
+            "Registration data placed at {:p}\n",
+            registration.open_data.as_ptr()
+        );
 
         pr_info!("Registered a new misc device `rchrdev`\n");
         Ok(())
     }
 
-    unsafe extern "C" fn open_callback(_inode: *mut inode, filp: *mut file) -> core::ffi::c_int {
+    unsafe extern "C" fn open_callback(inode: *mut inode, filp: *mut file) -> core::ffi::c_int {
         pr_info!("Called open_callback\n");
-        let ptr = crate::container_of!(unsafe { (*filp).private_data }, Self, miscdev);
-        unsafe { (*filp).private_data = ptr as *mut core::ffi::c_void };
-        0
+        pr_info!("file pointer private data at {:p}\n", unsafe {
+            (*filp).private_data
+        });
+
+        let reg = crate::container_of!(unsafe { (*filp).private_data }, Self, miscdev);
+        pr_info!("Registration data placed at {:p}\n", unsafe {
+            (*reg).open_data.as_ptr()
+        });
+        let open_data = unsafe { (*reg).open_data.as_ptr().as_ref().unwrap() };
+
+        if let Ok(data) = T::open(open_data) {
+            let ptr = ForeignOwnable::into_foreign(data);
+            unsafe { (*filp).private_data = ptr as *mut core::ffi::c_void };
+            pr_info!("Data from open will be placed at {:p}", unsafe {
+                (*filp).private_data
+            });
+            0
+        } else {
+            -(kernel::bindings::EFAULT as i32)
+        }
     }
 
     unsafe extern "C" fn read_callback(
@@ -125,7 +164,12 @@ impl<T: MiscDev> Registration<T> {
         ppos: *mut kernel::bindings::loff_t,
     ) -> isize {
         pr_info!("Called read_callback\n");
-        let device_buf = match T::read(count, unsafe { *ppos } as isize) {
+        pr_info!("file pointer private data at {:p}\n", unsafe {
+            (*filp).private_data
+        });
+        let mut data = unsafe { <T as MiscDev>::Data::borrow((*filp).private_data) };
+        pr_info!("Data for misc device placed at {:p}", &data);
+        let device_buf = match T::read(data, count, unsafe { *ppos } as isize) {
             Ok(rlen) => rlen,
             Err(err) => return -(err.to_errno() as isize),
         };
@@ -138,12 +182,66 @@ impl<T: MiscDev> Registration<T> {
             )
         };
         if res != 0 {
-            -(kernel::bindings::EFAULT as isize)
+            pr_err!("Problem while copying data to user space: {res}");
+            -(kernel::bindings::EINVAL as isize)
         } else {
             pr_info!("Read_response has {device_buf_len} bytes\n");
             unsafe { *filp }.f_pos += device_buf_len as i64;
             device_buf_len as isize
         }
+    }
+
+    unsafe extern "C" fn write_callback(
+        filp: *mut file,
+        buf: *const core::ffi::c_char,
+        count: usize,
+        ppos: *mut kernel::bindings::loff_t,
+    ) -> isize {
+        pr_info!("Called write_callback\n");
+        let mut data = unsafe { <T as MiscDev>::Data::borrow((*filp).private_data) };
+        pr_info!("Data for misc device placed at {:p}\n", &data);
+
+        let mut buffer = if let Ok(buffer) = Vec::try_with_capacity(count) {
+            buffer
+        } else {
+            pr_err!("Can't allocate {count} bytes\n");
+            return -(kernel::bindings::EFAULT as isize);
+        };
+        if buffer.try_resize(count, 0u8).is_err() {
+            pr_err!("Can't resize vector to {count} elements\n");
+            return -(kernel::bindings::EFAULT as isize);
+        }
+        let res = unsafe {
+            kernel::bindings::_copy_from_user(
+                buffer.as_slice().as_ptr() as *mut c_void,
+                buf as *const c_void,
+                count.try_into().unwrap(),
+            )
+        };
+        if res != 0 {
+            pr_err!("Problem while copying data from user space: {res}");
+            return -(kernel::bindings::EINVAL as isize);
+        }
+        pr_info!("Data buffer [{}]: {buffer:x?}\n", buffer.len());
+
+        match T::write(data, &buffer, unsafe { *ppos } as isize) {
+            Ok(wlen) => {
+                pr_info!("Write_response has {wlen} bytes\n");
+                unsafe { *filp }.f_pos += wlen as i64;
+                wlen
+            }
+            Err(err) => -(err.to_errno() as isize),
+        }
+    }
+
+    unsafe extern "C" fn release_callback(inode: *mut inode, filp: *mut file) -> core::ffi::c_int {
+        pr_info!("Called release_callback\n");
+        pr_info!("file pointer private data at {:p}\n", unsafe {
+            (*filp).private_data
+        });
+        let mut data = unsafe { <T as MiscDev>::Data::from_foreign((*filp).private_data) };
+        pr_info!("Data for misc device placed at {:p}", &data);
+        T::release(data).map(|_| 0).unwrap_or_else(Error::to_errno)
     }
 }
 
@@ -151,6 +249,7 @@ impl<T: MiscDev> Drop for Registration<T> {
     fn drop(&mut self) {
         if self.registered {
             unsafe { misc_deregister(&mut self.miscdev) };
+            unsafe { self.open_data.assume_init_drop() };
         }
     }
 }
@@ -169,8 +268,36 @@ impl<T: MiscDev> Drop for Registration<T> {
 ///
 /// ```
 pub trait MiscDev {
+    type Data: ForeignOwnable + Send + Sync;
+    type OpenData: Sync;
+
+    /// File opened
+    fn open(data: &Self::OpenData) -> crate::error::Result<Self::Data> {
+        Err(EINVAL)
+    }
+
     /// Returns the content of a read request
-    fn read(_count: usize, _pos: isize) -> crate::error::Result<Vec<u8>>;
+    fn read(
+        _context: <Self::Data as ForeignOwnable>::Borrowed<'_>,
+        _count: usize,
+        _pos: isize,
+    ) -> crate::error::Result<Vec<u8>> {
+        Err(EINVAL)
+    }
+
+    /// Returns the content of a read request
+    fn write(
+        _context: <Self::Data as ForeignOwnable>::Borrowed<'_>,
+        _data: &[u8],
+        _pos: isize,
+    ) -> crate::error::Result<isize> {
+        Err(EINVAL)
+    }
+
+    /// File closed
+    fn release(_context: Self::Data) -> Result<()> {
+        Err(EINVAL)
+    }
 }
 
 /// Calculates the offset of a field from the beginning of the struct it belongs to.
--- samples/rust/rust_chrdev.rs
+++ samples/rust/rust_chrdev.rs
@@ -6,6 +6,11 @@ use core::pin::Pin;
 use alloc::boxed::Box;
 use kernel::miscdev;
 use kernel::prelude::*;
+use kernel::{
+    new_mutex, new_spinlock,
+    sync::{Arc, ArcBorrow, Mutex, SpinLock, UniqueArc},
+    types::ForeignOwnable,
+};
 
 module! {
     type: RustChrdev,
@@ -15,31 +20,75 @@ module! {
     license: "GPL",
 }
 
-struct RustChrdev(Pin<Box<miscdev::Registration<ChrdevCallback>>>);
+#[pin_data]
+struct State {
+    #[pin]
+    buffer: Mutex<Vec<u8>>,
+}
 
-unsafe impl Sync for RustChrdev {}
+impl State {
+    fn try_new() -> Result<Arc<Self>> {
+        let mut data = Vec::new();
+        for i in "Hello CLT\n".as_bytes().iter() {
+            data.try_push(*i)?;
+        }
 
-struct ChrdevCallback;
+        let data = pin_init!(Self {
+            buffer <- new_mutex!(data),
+        });
+        Arc::pin_init(data)
+    }
+}
 
-impl miscdev::MiscDev for ChrdevCallback {
-    fn read(count: usize, ppos: isize) -> Result<Vec<u8>> {
-        let mut res = Vec::new();
+struct Callback {}
+
+impl miscdev::MiscDev for Callback {
+    type Data = Arc<State>;
+    type OpenData = Arc<State>;
+
+    fn open(open_data: &Self::OpenData) -> Result<Self::Data> {
+        pr_info!("Open data located at {:p}", open_data);
+        Ok(open_data.clone())
+    }
+
+    fn read(context: ArcBorrow<'_, State>, count: usize, ppos: isize) -> Result<Vec<u8>> {
+        pr_info!("Context data points to {:p}", &context);
         pr_info!("Got read request for {count} bytes from position {ppos}");
-        if ppos == 0 {
-            res.try_push(0x30)?;
-            res.try_push(0x0a)?;
+        let mut res = Vec::new();
+        let mut buffer = context.buffer.lock();
+        let len = core::cmp::min(count, buffer.len());
+        for i in buffer.drain(..len) {
+            res.try_push(i)?;
         }
         Ok(res)
     }
+
+    fn write(
+        context: <Self::Data as ForeignOwnable>::Borrowed<'_>,
+        data: &[u8],
+        _pos: isize,
+    ) -> kernel::error::Result<isize> {
+        let mut buffer = context.buffer.lock();
+        for i in data.iter() {
+            buffer.try_push(*i)?
+        }
+        Ok(data.len().try_into().unwrap())
+    }
 }
+
+struct RustChrdev {
+    registration: Pin<Box<miscdev::Registration<Callback>>>,
+}
+
+unsafe impl Sync for RustChrdev {}
 
 impl kernel::Module for RustChrdev {
     fn init(_module: &'static ThisModule) -> Result<Self> {
         pr_info!("Rust device driver init\n");
         pr_info!("*module = {:p}\n", _module);
-        let mut reg = miscdev::Registration::<ChrdevCallback>::new_pinned()?;
-        miscdev::Registration::register(reg.as_mut())?;
-        Ok(RustChrdev(reg))
+        let state = State::try_new()?;
+        let registration = miscdev::Registration::new_pinned(state)?;
+        Ok(RustChrdev { registration })
     }
 }
 
