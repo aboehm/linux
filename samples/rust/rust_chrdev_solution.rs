e4223bfea9ea1c971417e96c695beb1160c6d0a0
Alexander Böhm <alexander.boehm@malbolge.net>
2024-03-19 16:22:41 +0100

    Added rust character device solution for workshop goal

---
+++ samples/rust/rust_chrdev_solution.rs
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust character device sample.
+//!
+//! Solution for the workshop task.
+use core::pin::Pin;
+
+use alloc::boxed::Box;
+use kernel::prelude::*;
+use kernel::{
+    miscdev, new_mutex,
+    sync::{Arc, ArcBorrow, Mutex},
+};
+
+module! {
+    type: RustChrdev,
+    name: "rust_chrdev_solution",
+    author: "Alexander Böhm",
+    description: "Rust character device sample",
+    license: "GPL",
+}
+
+#[pin_data]
+struct Context {
+    #[pin]
+    buffer: Mutex<Vec<u8>>,
+}
+
+impl Context {
+    fn try_new() -> Result<Arc<Self>> {
+        let mut data = Vec::new();
+        for i in "Hello CLT\n".as_bytes().iter() {
+            data.try_push(*i)?;
+        }
+
+        let mutex = pin_init!(Context {
+            buffer <- new_mutex!(data),
+        });
+        Arc::pin_init(mutex)
+    }
+}
+
+struct Callback {}
+
+impl miscdev::MiscDev for Callback {
+    type Data = Arc<Context>;
+    type OpenData = Arc<Context>;
+
+    fn open(open_data: &Arc<Context>) -> Result<Arc<Context>> {
+        pr_info!("Open data located at {:p}", open_data);
+        Ok(open_data.clone())
+    }
+
+    fn read(context: ArcBorrow<'_, Context>, count: usize, ppos: isize) -> Result<Vec<u8>> {
+        pr_info!("Context data points to {:p}", &context);
+        pr_info!("Got read request for {count} bytes from position {ppos}");
+        let mut res = Vec::new();
+        let mut buffer = context.buffer.lock();
+        let len = core::cmp::min(count, buffer.len());
+        for i in buffer.drain(..len) {
+            res.try_push(i)?;
+        }
+        Ok(res)
+    }
+
+    fn write(context: ArcBorrow<'_, Context>, data: &[u8], _pos: isize) -> Result<isize> {
+        let mut buffer = context.buffer.lock();
+        for i in data.iter() {
+            buffer.try_push(*i)?
+        }
+        Ok(data.len().try_into().unwrap())
+    }
+}
+
+struct RustChrdev {
+    _registration: Pin<Box<miscdev::Registration<Callback>>>,
+}
+
+impl kernel::Module for RustChrdev {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        pr_info!("Rust device driver init\n");
+        pr_info!("*module = {:p}\n", _module);
+        let state = Context::try_new()?;
+        let registration = miscdev::Registration::new_pinned_registered(state)?;
+        Ok(RustChrdev {
+            _registration: registration,
+        })
+    }
+}
+
+impl Drop for RustChrdev {
+    fn drop(&mut self) {
+        pr_info!("Rust device driver exit\n");
+        pr_info!("*module = {:p}\n", self);
+    }
+}
